54f006524c8186545f84b9f3cb876596711e482b
Muhammad Kamran <smkamranqadri@yahoo.com>
2021-01-25 17:29:13 +0500

    recordrtc added with electron

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    backgroundColor: '#ffffff'
+  })
+  win.loadURL(`file://${__dirname}/dist/voice-notes/index.html`)
+  win.webContents.openDevTools()
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (win === null) {
+    createWindow()
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { AudioRecordingService } from './audio-recording.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'voice-notes';
+export class AppComponent implements OnDestroy {
+  isRecording = false;
+  recordedTime: string;
+  blobUrl;
+
+  constructor(private audioRecordingService: AudioRecordingService, private sanitizer: DomSanitizer) {
+
+    this.audioRecordingService.recordingFailed().subscribe(() => {
+      this.isRecording = false;
+    });
+
+    this.audioRecordingService.getRecordedTime().subscribe((time) => {
+      this.recordedTime = time;
+    });
+
+    this.audioRecordingService.getRecordedBlob().subscribe((data) => {
+      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(data.blob));
+    });
+  }
+
+  startRecording() {
+    if (!this.isRecording) {
+      this.isRecording = true;
+      this.audioRecordingService.startRecording();
+    }
+  }
+
+  abortRecording() {
+    if (this.isRecording) {
+      this.isRecording = false;
+      this.audioRecordingService.abortRecording();
+    }
+  }
+
+  stopRecording() {
+    if (this.isRecording) {
+      this.audioRecordingService.stopRecording();
+      this.isRecording = false;
+    }
+  }
+
+  clearRecordedData() {
+    this.blobUrl = null;
+  }
+
+  ngOnDestroy(): void {
+    this.abortRecording();
+  }
 }
